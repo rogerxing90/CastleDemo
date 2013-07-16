aebc4b5f852a0efec035456547edc19ac2268965
roger <rogerxing90@gmail.com>
2013-07-16 16:40:46 +0800

    Create new CastleDemo repo
    Castle learning demo

---
+++ CastleConsole/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+using CastleDemo;
+
+namespace CastleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("现在是Castle的代理在运行");
+            ProxyGenerator generator = new ProxyGenerator();
+            object proxy = generator.CreateClassProxy(typeof(RealA), new ProxyInterceptor());
+            (proxy as RealA).DoSomething();
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ CastleDemo/ProxyInterceptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.DynamicProxy;
+
+namespace CastleDemo
+{
+    public class ProxyInterceptor :StandardInterceptor
+    {
+        protected override void PreProceed(IInvocation invocation)
+        {
+            Console.WriteLine("Now I'm starting " + invocation.Method.Name);
+            base.PreProceed(invocation);
+        }
+
+        protected override void PostProceed(IInvocation invocation)
+        {
+            Console.WriteLine("Now I'm ending " + invocation.Method.Name);
+            base.PostProceed(invocation);
+        }
+    }
+}
